ffe776e5c69b433552a49fb3b806196e3c0a5a89
DIVYA-git123 <72966656+DIVYA-git123@users.noreply.github.com>
2020-10-27 11:33:07 +0530

    Add files via upload
    Java program to add two numbers.

---
+++ Addnumb.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Addnumb {
+
+	public static void main(String[] args) {
+		int a,b,s;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two numbers : ");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		s=a+b;
+		System.out.println("Sum is : "+s);
+		
+
+	}
+
+}
+
+Output:
+Enter two numbers :
+2
+3
+Sum is : 5
